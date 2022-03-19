9560b155687da72be029967fb58ba021b066ef92
bettybop29 <babyimoneter@gmail.com>
2022-03-19 14:10:39 +0700

    first commit

---
+++ src/pages/Layout/ContentFooter.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <nav>
+        <ul>
+          <li>
+            <a href=""> About Us </a>
+          </li>
+          <li>
+            <a href=""> Blog </a>
+          </li>
+          <li>
+            <a href=""> Licenses </a>
+          </li>
+        </ul>
+      </nav>
+      <div class="copyright text-center">
+        &copy; {{ new Date().getFullYear() }}
+        <a href="https://www.creative-tim.com/?ref=mdf-vuejs" target="_blank"
+          >Agri</a
+        >, made with <i class="fa fa-heart heart"></i> for a better web
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
---
+++ src/pages/TableList.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="content">
+    <div class="md-layout">
+      
+      
+
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+      <md-button class="md-success">Add Job</md-button>
+      <stats-card data-background-color="blue" v-for="item in list" v-bind:key="item.id">
+          <template slot="header">
+            <md-icon>work_outline</md-icon>
+          </template>
+
+          <template slot="content">
+            <h3 class="title">{{item.jobName}}</h3>
+            <p class="category">{{item.jobPosition}}</p>
+            <md-button class="md-success">Add Job</md-button>
+            
+          </template>
+
+          <template slot="footer">
+            <div class="stats">
+              <md-icon>account_balance_wallet</md-icon>
+              Rp. {{item.jobSalary}}
+            </div>
+          </template>
+        </stats-card>
+      </div>
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+        <md-card>
+          <md-card-header data-background-color="green">
+            <h4 class="title">Simple Table</h4>
+            <p class="category">Here is a subtitle for this table</p>
+          </md-card-header>
+          <md-card-content>
+            <simple-table table-header-color="green"></simple-table>
+          </md-card-content>
+        </md-card>
+      </div>
+
+      <div
+        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-100"
+      >
+        <md-card class="md-card-plain">
+          <md-card-header data-background-color="green">
+            <h4 class="title">Table on Plain Background</h4>
+            <p class="category">Here is a subtitle for this table</p>
+          </md-card-header>
+          <md-card-content>
+            <ordered-table></ordered-table>
+          </md-card-content>
+        </md-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { SimpleTable, OrderedTable, StatsCard } from "@/components";
+import axios from 'axios'
+
+export default {
+  components: {
+    OrderedTable,
+    SimpleTable,
+    StatsCard,
+  },
+  data() {
+    return {
+
+      list:[],
+      
+      
+    };
+  },
+   mounted(){
+    
+    axios.get(`http://54.255.4.75:9091/api/v1/jobs/5`)
+    .then((resp)=>{
+      this.list=resp.data
+      console.log(resp)
+      
+      
+    })
+    
+  }
+};
+</script>
